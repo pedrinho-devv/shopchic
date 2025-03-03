a5983db17b5d3d07176fa12ae0f67847b77752a4
PedroAugusto <pedrodesousakiske@gmail.com>
2025-03-03 10:34:16 -0300

    fazendo o hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -22,20 +22,27 @@ export default async function Hero() {
         alt="Imagem de destaque"
         layout="fill"
         objectFit="cover"
+        priority
         className="absolute top-0 left-0"
       />
 
-<div className="absolute top-1/2 left-12 transform -translate-y-1/2 px-6 py-4">
-  <h1 className="text-5xl md:text-6xl text-white font-extrabold mb-4">
-    The best games are here!
-  </h1>
-  <p className="text-xl md:text-2xl text-white font-medium">
-    For an unforgettable gaming experience, you’re in the right place.
-  </p>
-</div>
+      <div className="absolute top-1/2 left-12 transform -translate-y-1/2 px-6 py-4">
+        <h1 className="text-5xl md:text-6xl text-white font-extrabold mb-4">
+          The best <span className="text-[#AFE619]">Games</span><br /> are here!
+        </h1>
+        <p className="text-xl md:text-2xl text-white font-light mb-6">
+          For an unforgettable gaming experience, you’re in the right place.
+        </p>
+      </div>
 
-
-
+      <div className="absolute top-2/3 left-64 transform -translate-x-1/2 px-6 py-4 flex justify-center gap-4">
+        <button className="bg-transparent  font-black text-2xl text-[#AFE619] border border-[#AFE619] px-8 py-4 rounded-sm hover:bg-[#AFE619] hover:text-white duration-200 ease-in-out">
+          See More
+        </button>
+        <button className="bg-[#AFE619] font-black text-2xl text-white rounded-2xl px-8 py-4 rounded-sm border border-transparent hover:bg-transparent hover:border-[#AFE619] hover:text-[#AFE619] duration-200 ease-in-out">
+          Shop Now
+        </button>
+      </div>
     </section>
   );
 }
