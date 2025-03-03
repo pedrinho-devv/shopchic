f7a82f74067c58f50d921ac6e237ac79a30e47b8
PedroAugusto <pedrodesousakiske@gmail.com>
2025-03-03 10:40:48 -0300

    tirando o any

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,17 +1,19 @@
-import {createClient} from 'next-sanity'
+import { createClient } from 'next-sanity'
 import imageURLBuilder from '@sanity/image-url'
 
-
+// Tipo de dado para a imagem do Sanity
+import { Image } from '@sanity/types' // Importando o tipo correto, caso disponível
 
 export const client = createClient({
-    projectId:'zzvakq3o',
-    dataset:'production',
+    projectId: 'zzvakq3o',
+    dataset: 'production',
     apiVersion: '2025-03-01',
     useCdn: true,
 })
 
 const builder = imageURLBuilder(client)
 
-export function urlFor(source: any){
+// Especificando o tipo para o parâmetro 'source'
+export function urlFor(source: Image) {
     return builder.image(source)
 }
